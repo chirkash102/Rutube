130ba13bd2ac0477d752af30cfaf62dfce3be3d6
dderevyanchenko <xnauutilus2x@yandex.ru>
2023-09-19 16:56:27 +0300

    changes from office

--- app/src/main/java/com/example/rutube/fragments/FragmentHome.kt
+++ app/src/main/java/com/example/rutube/fragments/FragmentHome.kt
@@ -5,12 +5,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.paint
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.res.painterResource
 import androidx.fragment.app.Fragment
+import com.example.rutube.R
 import com.example.rutube.transaction
 import com.example.rutube.ui.theme.RutubeTheme
+import com.example.rutube.ui.theme.Shapes
 
 class FragmentHome : Fragment() {
 
@@ -37,9 +46,19 @@ class FragmentHome : Fragment() {
 fun MainScreen(
     onClick: () -> Unit
 ) {
-    Box() {
-        Button(onClick = onClick) {
+    Box(contentAlignment = Alignment.BottomCenter,
+        modifier = Modifier
+            .fillMaxSize()
+            .paint(
+                painterResource(id = R.drawable.putin),
+                contentScale = ContentScale.Crop
 
+            ),
+
+
+    ) {
+        Button(shape = Shapes.small,onClick = onClick,) {
+Text(text = "START")
         }
     }
 
