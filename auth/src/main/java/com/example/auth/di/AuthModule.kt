e5237c018739f28f729000b0b106658114f8efea
Dimas <xnautilus2x@yandex.ru>
2023-10-01 19:06:03 +0300

    ass sec table + likeFunctionality

--- auth/src/main/java/com/example/auth/di/AuthModule.kt
+++ auth/src/main/java/com/example/auth/di/AuthModule.kt
@@ -5,6 +5,7 @@ import com.example.auth.data.RutubeRepository
 import com.example.auth.data.RutubeAuthRepositoryImpl
 import com.example.auth.roommodel.AppDataBase
 import com.example.auth.viewmodel.RutubeViewModel
+import com.example.likescreen.rommtable.LikeDao
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
@@ -12,6 +13,10 @@ val authModule = module {
 
     single<AppDataBase> {
         Room.databaseBuilder(get(), AppDataBase::class.java, "RutubeDataBase").build()
+    }
+    single<LikeDao> {
+        val dataBase = get<AppDataBase>()
+        dataBase.getLikeDao()
     }
     single<RutubeRepository> { RutubeAuthRepositoryImpl(get()) }
     viewModel { RutubeViewModel(get()) }
--- top20videos/src/main/kotlin/com/example/top20videos/di/Top20Module.kt
+++ top20videos/src/main/kotlin/com/example/top20videos/di/Top20Module.kt
@@ -1,6 +1,8 @@
 package com.example.top20videos.di
 
 import com.example.auth.data.RutubeRepository
+import com.example.likescreen.repository.LikeRepository
+import com.example.likescreen.repository.LikeRepositoryImpl
 import com.example.top20videos.repository.Top20Repository
 import com.example.top20videos.repository.Top20RepositoryImpl
 import com.example.top20videos.retrofit.RutubeApi
@@ -12,8 +14,10 @@ import org.koin.dsl.module
 val top20module = module {
     single<RutubeApi> { RutubeRetrofit.rutubeApi }
     single<Top20Repository> { Top20RepositoryImpl(get()) }
+    single<LikeRepository> { LikeRepositoryImpl(get()) }
+
    // viewModel { RutubeRetrofitViewModel(get())}
     viewModel {
-        RutubeRetrofitViewModel(get(), get())
+        RutubeRetrofitViewModel(get(), get(), get())
     }
 }
