27715401fe077025a32d518f9e92fc826dad15e5
Dimas <xnautilus2x@yandex.ru>
2023-10-03 16:59:36 +0300

    add primarykey to fix crash

--- localdatasource/src/main/java/com/example/localdatasource/entity/LikeVideo.kt
+++ localdatasource/src/main/java/com/example/localdatasource/entity/LikeVideo.kt
@@ -1,10 +1,14 @@
 package com.example.localdatasource.entity
 
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
-@Entity(primaryKeys = ["login","title"]) //сдвоенный ключ
+@Entity //сдвоенный ключ
 data class LikeVideo(
+
     val login:String,
     val thumbnailUrl:String,
-    val title:String
+    val title:String,
+    @PrimaryKey(autoGenerate = true)
+val id: Int = 0,
 )
