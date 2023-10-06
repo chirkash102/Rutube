9035e733405177587c925896de5eca99b1948192
Dimas <xnautilus2x@yandex.ru>
2023-10-06 18:27:10 +0300

    add callbeck without adding in main

--- auth/src/main/java/com/example/auth/fragments/FragmentProfile.kt
+++ auth/src/main/java/com/example/auth/fragments/FragmentProfile.kt
@@ -1,12 +1,22 @@
 package com.example.auth.fragments
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.Fragment
+import com.example.auth.viewmodel.RutubeViewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class FragmentProfile : Fragment() {
+    private var callBack: RutubeProfileNavigation? = null
+    private val viewModel: RutubeViewModel by viewModel()
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        callBack = (requireActivity() as? RutubeProfileNavigation)
+    }
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -14,5 +24,12 @@ class FragmentProfile : Fragment() {
     ) = ComposeView(requireContext()).apply {
         setContent {}
 
+    }
+    interface RutubeProfileNavigation {
+        fun navigateToTopVideosFromRegistration()
+    }
+    override fun onDetach() {
+        super.onDetach()
+        callBack = null
     }
 }
